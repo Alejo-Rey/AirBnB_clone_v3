9f16369db0394df97df9831d56675d1bb9ad1bd7
javb92 <mentor37_@hotmail.com>
2019-09-11 02:40:53 +0000

    [FEATURE] added route that let you show stats about classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,26 @@
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def json_return():
+    """ return json
+    """
     return jsonify({
                     "status": "OK"
                     })
+
+
+@app_views.route('/stats')
+def stat_return():
+    """statistic return
+    """
+    clas_stat = {"amenities": storage.count("Amenity"),
+                 "cities": storage.count("City"),
+                 "places": storage.count("Place"),
+                 "reviews": storage.count("Review"),
+                 "states": storage.count("State"),
+                 "users": storage.count("User")}
+    return jsonify(clas_stat)
